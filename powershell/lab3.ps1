9ad6dba6f5628e1e19c62a57ea4673f111a008d0
unknown <bintzhm@mail.uc.edu>
2021-01-27 11:39:03 -0500

    adding powershell files

---
+++ powershell/lab3.ps1
@@ -0,0 +1,12 @@
+﻿function getIP {
+    (get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+$IP = getIP
+$User = $env:username
+$Date = Get-Date -DisplayHint Date
+$Hostname = hostname
+$PWSH = $HOST.Version.Major
+
+$Body = "This machine's IP address is $IP. User is $User. Hostname is $Hostname. Powershell version is $PWSH. Today's date is $Date"
+
+write-host($Body)
