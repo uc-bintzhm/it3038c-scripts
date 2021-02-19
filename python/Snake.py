322aa25170059abf6fbe332000d4028bc2221365
uc-bintzhm <bintzhm@mail.uc.edu>
2021-02-19 18:06:40 -0500

    New Change

--- python/Snake.py
+++ python/Snake.py
@@ -42,9 +42,18 @@ def gameLoop(): #creating a function
 
     while not game_over:
         while game_close == True:
-            message("You lost", red)
+            message("You lost. Press 'q' to Quit and press 'p' to Play again.", red) # give users option to play again or not
             pygame.display.update()
-            time.sleep(2)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q: #has q quit
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_p: # has p play again
+                        gameLoop()
+
+
         for event in pygame.event.get():
             if event.type==pygame.QUIT: #allows you to quit the game
                 game_over=True
@@ -62,7 +71,7 @@ def gameLoop(): #creating a function
                     x1_change = 0
                     y1_change = snake_block
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 <0:
-            game_over=True
+            game_close=True
         
         x1 += x1_change
         y1 += y1_change
@@ -71,6 +80,10 @@ def gameLoop(): #creating a function
         pygame.draw.rect(dis,blue,[x1,y1,snake_block,snake_block]) #sets the starting point with a blue color/snake
         
         pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            print("Yeet.") # has code acknowledge food
+
 
         clock.tick(snake_speed)
 
